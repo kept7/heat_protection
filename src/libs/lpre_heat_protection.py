fdd29cafc1d437b9a336def3b2cc46f7b863318d
Denis Gorodetskiy <denez.gorodetskii@gmail.com>
2024-10-31 22:49:58 +0300

    main - added calc of chamber params; added func of writing to xlsx

--- src/libs/lpre_heat_protection.py
+++ src/libs/lpre_heat_protection.py
@@ -1,2 +1,63 @@
+from typing import List
+from numpy import pi, sqrt
+from collections import deque
+
+
 def hello_world():
     print("hello world")
+
+
+def chamber_params(
+    x_coord_list: List[float], d_list: List[float], D_kp: float, F_kp: float
+) -> List[List[int]]:
+
+    D_ratio_list = [round(i / D_kp, 6) for i in d_list]
+    F_list = [round((pow(i, 2) * pi) / 4, 6) for i in d_list]
+    F_ratio_list = [round(i / F_kp, 6) for i in F_list]
+    Delta_x_list = [
+        round(el - x_coord_list[i], 6) for i, el in enumerate(x_coord_list[1:])
+    ]
+    Delta_x_list = deque(Delta_x_list)
+    Delta_x_list.appendleft("-")
+    Delta_x_list = list(Delta_x_list)
+    r_from_d = [i / 2 for i in d_list]
+    Delta_xs_list = [
+        round(
+            sqrt(
+                pow(x_coord_list[i + 1] - x_coord_list[i], 2)
+                + pow(r_from_d[i + 1] - r_from_d[i], 2)
+            ),
+            6,
+        )
+        for i, _ in enumerate(d_list[1:])
+    ]
+    Delta_S = [
+        round(0.5 * pi * (d_list[i] + d_list[i + 1]) * Delta_xs_list[i], 6)
+        for i, el in enumerate(Delta_xs_list[1:])
+    ]
+
+    Delta_xs_list = deque(Delta_xs_list)
+    Delta_xs_list.appendleft("-")
+    Delta_xs_list = list(Delta_xs_list)
+    Delta_xs_list.append("-")
+
+    Delta_S = deque(Delta_S)
+    Delta_S.appendleft("-")
+    Delta_S = list(Delta_S)
+    Delta_S.append("-")
+
+    result = [
+        [
+            x_coord_list[i],
+            d_list[i],
+            D_ratio_list[i],
+            F_list[i],
+            F_ratio_list[i],
+            Delta_x_list[i],
+            Delta_xs_list[i],
+            Delta_S[i],
+        ]
+        for i, _ in enumerate(x_coord_list)
+    ]
+
+    return result
--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ from os import getenv
 from pathlib import Path
 from typing import List
 from dotenv import load_dotenv
-from pandas import read_excel
+from pandas import read_excel, DataFrame, ExcelWriter
+from matplotlib.pyplot import plot, show
+from scipy.interpolate import interp1d
 import libs.lpre_heat_protection as hp
 
 """
@@ -19,19 +21,46 @@ Init data:
 
 
 def main_programm() -> None:
-    # getting x and D coordinates
+    # getting x and D coordinates from excel file
     X_D_PATH_FILE = get_env_path("X_D_PATH_FILE")
 
-    x_coord = []
-    d_coord = []
+    x_coord_list = []
+    d_list = []
 
-    x_coord, d_coord = xlsx_data(X_D_PATH_FILE, x_coord, d_coord)
+    x_coord_list, d_list = get_xlsx_data(X_D_PATH_FILE, x_coord_list, d_list)
 
-    print(x_coord)
-    print(d_coord)
+    # getting parameters of the flow part of the chamber (header 1.3.1 - manual)
+    D_kp = 0.220165
+    F_kp = 0.03807
+    ch_res_res = hp.chamber_params(x_coord_list, d_list, D_kp, F_kp)
+
+    # writing the results to excel file
+    RESULT_PATH_FILE = get_env_path("RESULT_PATH_FILE")
+    column_names = [
+        "x",
+        "D",
+        "D OTH",
+        "F",
+        "F OTH",
+        "delta(x)",
+        "delta(xs)",
+        "delta(S)",
+    ]
+    write_xlsx_data(RESULT_PATH_FILE, column_names, "first_sheet", ch_res_res)
+
+    # r_from_d = [i / 2 for i in d_list]
+    # y_interp = interp1d(x_coord_list, r_from_d, kind="linear")
+    # # xnew = arange(x_coord_list[1], x_coord_list[-1], x_coord_list[-1] / 32)
+    # test = [(x_coord_list[i+1] - x_coord_list[i]) for i, el in enumerate(x_coord_list[1:])]
+    # print(test)
+    # ynew = y_interp(test)
+    # print(ynew)
+
+    # plot(x_coord_list[1:], ynew, 'o--')
+    # show()
 
 
-def get_env_path(env_name) -> str:
+def get_env_path(env_name: str) -> str:
     dotenv_path = Path("../.env")
     load_dotenv(dotenv_path=dotenv_path)
     path_file = getenv(env_name)
@@ -39,14 +68,25 @@ def get_env_path(env_name) -> str:
     return path_file
 
 
-def xlsx_data(path_file, first_col, second_col) -> List[float]:
+def get_xlsx_data(
+    path_file: str, first_col: List[None], second_col: List[None]
+) -> List[float]:
     df = read_excel(path_file, header=None)
 
     for _, row in df.iterrows():
-        first_col.append(row.tolist()[0])
-        second_col.append(row.tolist()[1])
+        first_col.append(round(row.tolist()[0], 6))
+        second_col.append(round(row.tolist()[1], 6))
 
     return first_col, second_col
+
+
+def write_xlsx_data(
+    PATH_FILE: str, column_names: str, sheet_number: str, result: List[List[int]]
+) -> None:
+    df = DataFrame(result, columns=column_names)
+    writer = ExcelWriter(PATH_FILE, engine="xlsxwriter")
+    df.to_excel(writer, sheet_name=sheet_number, index=False)
+    writer.close()
 
 
 if __name__ == "__main__":
