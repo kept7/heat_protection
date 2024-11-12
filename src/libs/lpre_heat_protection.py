1e970c19609b20a3136e743fde9fd6e70f09f17c
Denis Gorodetskiy <denez.gorodetskii@gmail.com>
2024-11-13 00:02:59 +0300

    main - cooling_path_params physics correction

--- src/libs/lpre_heat_protection.py
+++ src/libs/lpre_heat_protection.py
@@ -3,8 +3,6 @@ from collections import deque
 from math import floor
 from numpy import round, pi, sqrt, radians, sin, cos, tan
 
-# from scipy.interpolate import interp1d
-
 
 def hello_world() -> None:
     print("hello world")
@@ -28,15 +26,6 @@ def chamber_params(
     Delta_x_list.appendleft("-")
     Delta_x_list = list(Delta_x_list)
 
-    # How to find xs in correct meaning (via interp):
-    # r_from_d = [i / 2 for i in d_list]
-    # y_interp = interp1d(x_coord_list, r_from_d, kind="linear")
-    # # xnew = arange(x_coord_list[1], x_coord_list[-1], x_coord_list[-1] / 32)
-    # test = [(x_coord_list[i+1] - x_coord_list[i]) for i, el in enumerate(x_coord_list[1:])]
-    # print(test)
-    # ynew = y_interp(test)
-    # print(ynew)
-
     r_from_d = [diam / 2 for diam in d_list]
 
     Delta_xs_list = [
@@ -50,15 +39,15 @@ def chamber_params(
         for i, _ in enumerate(d_list[1:])
     ]
 
-    Delta_xs_list = deque(Delta_xs_list)
-    Delta_xs_list.appendleft("-")
-    Delta_xs_list = list(Delta_xs_list)
-    Delta_xs_list.append("-")
-
     Delta_S = [
         round(0.5 * pi * (d_list[i] + d_list[i + 1]) * Delta_xs_list[i], 6)
         for i, _ in enumerate(Delta_xs_list[1:])
     ]
+
+    Delta_xs_list = deque(Delta_xs_list)
+    Delta_xs_list.appendleft("-")
+    Delta_xs_list = list(Delta_xs_list)
+    Delta_xs_list.append("-")
 
     Delta_S = deque(Delta_S)
     Delta_S.appendleft("-")
@@ -84,52 +73,49 @@ def chamber_params(
 
 def cooling_path_params(d_list: List[float]) -> List[List[float]]:
     # TODO -> 1) get init data (h, beta, gamma and etc)
-    #         2) n_p_list calc
-    #         3) n_p bad res
-    #         4) FINAL RESULT MAY BE INCORRECT -> IT NEEDS RESEARCHES
+    #         2) decrease complexity of n_p_list calc
 
-    mode = "rightangle"
-    h = 3
-    delta_ct = 1
-    delta_p = 1
-    delta_ct_HAP = 3
-    beta = radians(15)
+    mode = "gofra"
+    h = 0.003
+    delta_ct = 0.001
+    delta_p = 0.001
+    delta_ct_HAP = 0.003
+    beta = radians(0)
     gamma = radians(90)
     h_p = (h - delta_p) / sin(gamma)
 
-    t_N_min = 2.5
+    t_N_min = 0.0025
 
     d_avg_list = [
         diam * (1 + (2 * delta_ct + h) / diam) for _, diam in enumerate(d_list)
     ]
     d_avg_min = min(d_avg_list)
-    d_kp = min(d_list)
 
     n_p_kp = floor(pi * d_avg_min * cos(beta) / t_N_min)
 
     n_p_min = round(n_p_kp / 2) if n_p_kp % 2 != 0 else n_p_kp
     t_N_min = pi * d_avg_min * cos(beta) / n_p_min
 
-    n_p_list = []
-    for _, el in enumerate(d_list):
-        # power_of = 0
-        # if el / d_min < pow(2, stepen):
-        #     n_p_kp * pow(2, stepen)
-        # else:
-        #     stepen += 1
-        if round(el / d_kp, 2) < 2:
-            n_p_list.append(n_p_min)
-        elif round(el / d_kp, 2) >= 2 and round(el / d_kp, 2) < 4:
-            n_p_list.append(n_p_min * 2)
-        elif round(el / d_kp, 2) >= 4 and round(el / d_kp, 2) < 8:
-            n_p_list.append(n_p_min * 4)
-        elif round(el / d_kp, 2) >= 8 and round(el / d_kp, 2) < 16:
-            n_p_list.append(n_p_min * 8)
-        elif round(el / d_kp, 2) >= 16 and round(el / d_kp, 2) < 32:
-            n_p_list.append(n_p_min * 16)
+    n_p_list = [n_p_min for _ in d_list]
 
-    t_list = [pi * diam_avg / n_p_list[i] for i, diam_avg in enumerate(d_avg_list)]
-    t_N_list = [t_val * cos(beta) for _, t_val in enumerate(t_list)]
+    t_list = []
+    t_N_list = []
+
+    for i, el in enumerate(n_p_list):
+        n_p_val = el
+        t_val = pi * d_avg_list[i] / n_p_val
+        t_N = t_val * cos(beta)
+
+        while t_N > 0.007:
+            n_p_val *= 2
+            t_val = pi * d_avg_list[i] / n_p_val
+            t_N = t_val * cos(beta)
+
+        if n_p_val != el:
+            n_p_list[i] = n_p_val
+
+        t_list.append(round(t_val, 4))
+        t_N_list.append(round(t_N, 4))
 
     if mode == "shelevoi":
         f_list = [pi * diam_avg * h for _, diam_avg in enumerate(d_avg_list)]
@@ -145,7 +131,7 @@ def cooling_path_params(d_list: List[float]) -> List[List[float]]:
             for _, t_N in enumerate(t_N_list)
         ]
         b_list = ["-" for _ in d_avg_list]
-    else:
+    elif mode == "gofra":
         b_list = [t_N - h / tan(gamma) for _, t_N in enumerate(t_N_list)]
         f_list = [
             n_p_list[i]
